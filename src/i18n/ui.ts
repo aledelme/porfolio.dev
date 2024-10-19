981f6f6eb0514167d61c94af98d8ffacaf938a8e
aledelme <alejandrodelmedico@gmail.com>
2024-10-20 00:56:53 +0200

    Avance i18n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,21 @@
+// export const languages = {
+//     es: 'Español',
+//     en: 'English',
+// };
+
+export const defaultLang = 'es';
+
+export const ui = {
+    es: {
+        'nav.experience': 'Experiencia',
+        'nav.about': 'Sobre mí',
+        'nav.contact': 'Contacto',
+        'nav.cv': 'CV'
+    },
+    en: {
+        'nav.experience': 'Experience',
+        'nav.about': 'About me',
+        'nav.contact': 'Contact',
+        'nav.cv': 'CV'
+    },
+} as const;
